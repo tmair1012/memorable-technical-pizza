34e32d5475c7ab53c9b04604c994f3ce8affb4db
TylerMair <tyleramair@gmail.com>
2022-10-23 13:38:28 -0700

    corrected auto increment for primary key

--- db.py
+++ db.py
@@ -14,15 +14,20 @@ mycursor = db.cursor()
 #mycursor.execute('CREATE DATABASE pizzadb')
 
 #pizza table creation
-#mycursor.execute('CREATE TABLE pizzas (id int PRIMARY KEY, pizza_name VARCHAR(50), created_by VARCHAR(50), timestamp TIMESTAMP)')
+#mycursor.execute('CREATE TABLE Pizzas (id int PRIMARY KEY AUTO_INCREMENT, pizza_name VARCHAR(50), timestamp TIMESTAMP)')
 
 #toppings table creation
-#mycursor.execute('CREATE TABLE toppings (id int PRIMARY KEY, topping_name VARCHAR(50), timestamp TIMESTAMP)')
+#mycursor.execute('CREATE TABLE Toppings (id int PRIMARY KEY AUTO_INCREMENT, topping_name VARCHAR(50), timestamp TIMESTAMP)')
 
-mycursor.execute('''CREATE TABLE pizza_masterpieces (
-    id int PRIMARY KEY,
-    pizzaID int,
-    toppingID int,
-    FOREIGN KEY (pizzaID) REFERENCES pizzas(id),
-    FOREIGN KEY (toppingID) REFERENCES toppings(id),
-    timestamp TIMESTAMP)''')
+#mycursor.execute('''CREATE TABLE Masterpieces (
+    #id int PRIMARY KEY AUTO_INCREMENT,
+    #pizzaID int,
+    #toppingID int,
+    #FOREIGN KEY (pizzaID) REFERENCES pizzas(id),
+    #FOREIGN KEY (toppingID) REFERENCES toppings(id),
+    #created_by VARCHAR(50),
+    #timestamp TIMESTAMP)''')
+
+#mycursor.execute('''INSERT INTO pizzas (pizza_name)
+#VALUES ('Buffalo Chicken')''')
+
