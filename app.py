6ca539b154f60f85c259fa393cea37a91b71b22d
TylerMair <tyleramair@gmail.com>
2022-10-23 17:51:40 -0700

    404 and 500 pages

--- app.py
+++ app.py
@@ -12,9 +12,26 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    mycursor.execute("SELECT * FROM Toppings")
+    mycursor.execute("SELECT topping_name FROM Toppings")
     data = mycursor.fetchall()
     return render_template('index.html', data=data)
+
+#@app.route("/updateToppings/<int:id>", methods=['GET', 'POST'])
+#def updateToppings(id):
+    
+#Bad URL page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+#Internal error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+
+    
+
 
 if __name__ == "__main__":
     app.run()
