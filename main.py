996312e93dce72e79535d3a513da6b2641b24a90
TylerMair <tyleramair@gmail.com>
2022-10-25 10:40:53 -0700

    add topping working sucessfully

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request, url_for, redirect
-app = Flask(__name__)
+from flask import Flask, render_template, request, url_for, redirect, flash
 import mysql.connector
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import Form, StringField, SubmitField, validators
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
 
+#create Flask instance
+app = Flask(__name__)
 
-#initialize Database
+
 #establish connection to mysql
 db = mysql.connector.connect(
     host='localhost',
@@ -21,12 +21,31 @@ db = mysql.connector.connect(
 #cursor creation
 mycursor = db.cursor()
 
+class ToppingForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+
+class PizzaForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)]) 
+
 #homepage route
 @app.route("/", methods=['GET'])
 def home():
+
+    #Get all Toppings
     mycursor.execute("SELECT * FROM Toppings")
     data = mycursor.fetchall()
     return render_template('index.html', data=data)
+
+    
+
+@app.route("/creation/<string:id>/")
+def article(id):
+
+    data = mycursor.execute("SELECT * FROM Pizzas WHERE id =%s", [id])
+
+    data = mycursor.fetchone()
+
+    return render_template('masterpiece.html', data=data)
 
 
 #@app.route("/updateToppings/<int:id>", methods=['GET', 'POST'])
@@ -57,14 +76,29 @@ def home():
 #Add a Topping
 @app.route('/topping/add', methods=['GET', 'POST'])
 def add_toppings():
-    mycursor.execute("SELECT topping_name FROM Toppings")
-    data = mycursor.fetchall()
-    if request.method == 'POST':
-        topping_name = request.form.get('topping_name')
-        query = "INSERT INTO Toppings(id, topping_name, timestamp) VALUES (NULL, %s, NOW())"
-        mycursor.execute(query, (topping_name,))
+    form = ToppingForm(request.form)
+    if request.method == 'POST' and form.validate():
+        topping_name = form.name.data
+
+        # Execute cursor
+        mycursor.execute("INSERT INTO Toppings(id, topping_name, timestamp) VALUES (NULL, %s, NOW())",(topping_name,))
+
+        # Commit to DB
         db.commit()
-    return render_template('topping.html', data=data)
+
+        #Success Message
+        flash('Topping Created', 'success')
+
+        return redirect(url_for("home"))
+    return render_template('add_topping.html', form=form)
+    #mycursor.execute("SELECT topping_name FROM Toppings")
+    #data = mycursor.fetchall()
+    #if request.method == 'POST':
+        #topping_name = request.form.get('topping_name')
+        #query = "INSERT INTO Toppings(id, topping_name, timestamp) VALUES (NULL, %s, NOW())"
+        #mycursor.execute(query, (topping_name,))
+        #db.commit()
+   # return render_template('topping.html', data=data)
 
 #Add a Pizza
 @app.route('/pizza/add', methods=['GET', 'POST'])
@@ -110,4 +144,6 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
+    app.secret_key='shhhhh smiley face'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(port=5200)
